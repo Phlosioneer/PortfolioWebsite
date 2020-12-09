88c69f98050b11738369c8f6cbd6ee6786ca4e50
Phlosioneer <mattmdrr2@gmail.com>
2020-12-08 21:19:16 -0500

    Simplify host script
    Remove all the cache header stuff. Trust the browser to not cache
    stuff from localhost.

--- src/script/host.js
+++ src/script/host.js
@@ -1,19 +1,13 @@
 "use strict";
 
-const path = require("path");
+const path = require('path');
 const express = require("express");
-
-
 const app = express();
-const staticOptions = {
-	setHeaders: resp => resp.set("Cache-Control", "no-store")
-};
+
 const project_root = path.dirname(path.dirname(__dirname));
+
 const sendFileOptions = {
-	root: project_root,
-	headers: {
-		"Cache-Control": "no-store"
-	}
+	root: project_root
 };
 
 app.use("/", (req, resp, next) => {
@@ -25,9 +19,12 @@ app.use("/", (req, resp, next) => {
 });
 
 app.get("/", (req, resp) => resp.sendFile("src/web/index.html", sendFileOptions));
-app.use("/", express.static("src/web", staticOptions));
-app.use("/node_modules/bulma/sass", express.static("node_modules/bulma/sass", staticOptions));
-app.get("/node_modules/bulma/bulma.sass", (req, resp) => resp.sendFile("node_modules/bulma/bulma.sass", sendFileOptions));
+app.use("/", express.static("src/web"));
+app.use("/node_modules/bulma/sass", express.static("node_modules/bulma/sass"));
+app.get("/node_modules/bulma/bulma.sass", (req, resp) => {
+	resp.sendFile("node_modules/bulma/bulma.sass", sendFileOptions);
+});
+app.use("/sass", express.static("src/sass"));
 
 app.use("/", (req) => console.log("Unhandled request!", {
 	url: req.originalUrl,
