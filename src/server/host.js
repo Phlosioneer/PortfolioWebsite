170a789f4f2ebe28bb3ab97a705493fdd7babd3c
Phlosioneer <mattmdrr2@gmail.com>
2020-12-31 22:15:36 -0500

    Add external link icon to resume button

--- src/server/host.js
+++ src/server/host.js
@@ -18,9 +18,15 @@ app.use("/", (req, resp, next) => {
 	next();
 });
 
+// Website
 app.get("/", (req, resp) => resp.sendFile("generated/web/index.html", sendFileOptions));
 app.use("/", express.static("generated/web"));
 app.use("/", express.static("resources/images"));
+
+// Dependencies
+const faPath = "node_modules/@fortawesome/fontawesome-free"
+app.use("/fontawesome/", express.static(path.join(faPath, "css")));
+app.use("/webfonts/", express.static(path.join(faPath, "webfonts")));
 app.get("/splide.js", (req, resp) => resp.sendFile("node_modules/@splidejs/splide/dist/js/splide.js", sendFileOptions));
 app.get("/splide.min.css", (req, resp) => resp.sendFile("node_modules/@splidejs/splide/dist/css/themes/splide-default.min.css", sendFileOptions));
 
@@ -30,11 +36,19 @@ app.get("/node_modules/bulma/bulma.sass", (req, resp) => {
 	resp.sendFile("node_modules/bulma/bulma.sass", sendFileOptions);
 });
 app.use("/src/sass", express.static("src/sass"));
+
+// Favicon not implemented
 app.get("/favicon.ico", (req, resp) => resp.sendStatus(404));
-app.use("/", (req) => console.log("Unhandled request!", {
-	url: req.originalUrl,
-	method: req.method
-}));
+
+
+
+app.use("/", (req, resp) => {
+	console.log("Unhandled request!", {
+		url: req.originalUrl,
+		method: req.method
+	});
+	resp.sendStatus(501);
+});
 
 app.listen(8080, () => {
 	console.log("Listening at http://localhost:8080");
